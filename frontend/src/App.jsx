e43e9371e01bebdac2f58236481ba858f93b749b
KacperOmen <komen1@stu.vistula.edu.pl>
2025-11-16 22:26:54 +0100

    Import stron Login i Signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import {Route, Routes} from'react-router-dom';
 import ClientsPage from "./pages/ClientsPage";
 import HomePage from './pages/HomePage';
 import {ToastContainer} from 'react-toastify';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
