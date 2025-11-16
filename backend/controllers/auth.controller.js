a5d6e469ec05c9673896ad3042cf09888b447c69
KacperOmen <komen1@stu.vistula.edu.pl>
2025-11-16 21:46:55 +0100

    Dodano funkcję login w kontrolerze auth

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -25,3 +25,35 @@ export const register = async (req, res) => {
         res.status(500).json({success: false, message: error.message});
     }
 }
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({success: false, message: "Email and password are required"});
+        }
+
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(401).json({success: false, message: "Invalid email or password"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({success: false, message: "Invalid email or password"});
+        }
+
+        const token = jwt.sign({id: user._id}, "secret", {expiresIn: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        })
+
+        return res.status(200).json({success: true, user});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message});
+    }
+}
