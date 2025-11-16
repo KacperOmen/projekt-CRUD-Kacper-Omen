7c1b4515ced5476acb2d7048a855841d552d488e
KacperOmen <komen1@stu.vistula.edu.pl>
2025-11-16 22:19:56 +0100

    Skonfigurowano routing aplikacji w App oraz dodano ToastContainer w celu obsługi powiadomień

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import {Route, Routes} from'react-router-dom';
 import ClientsPage from "./pages/ClientsPage";
+import HomePage from './pages/HomePage';
+import {ToastContainer} from 'react-toastify';
 
 function App() {
   return (
     <div>
-      <ClientsPage />
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        
+        <Route 
+          path='/clients' 
+          element={<ClientsPage />} 
+        />
+      </Routes>
     </div>
   );
 }
