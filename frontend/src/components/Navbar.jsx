f5ea808b034f9c9ab7f0e0d273a4841b50586b96
KacperOmen <komen1@stu.vistula.edu.pl>
2025-12-02 13:29:39 +0100

    Zmiana tekstu nagłówka oraz poprawa kodu statusu w postClient

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="w-full flex justify-between items-center border-b-2 p-4 bg-white shadow-md">
       <h1 className="text-2xl font-bold text-gray-800">
-        Moja aplikacja TEST
+        Moja aplikacja
       </h1>
 
       <div className="flex items-center">
